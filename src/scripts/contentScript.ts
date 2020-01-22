563733b4714211e239cfa82e5c2e8bdca52a7ab9
cheongan <smartnation@naver.com>
2020-01-23 04:58:15 +0900

    feat: ⚡️ Add Name Fillter

--- src/scripts/contentScript.ts
+++ src/scripts/contentScript.ts
@@ -1,14 +1,19 @@
-document.getElementById('cafe_main').onload = function() {  
-  let body = iframeRef(document.getElementById('cafe_main'))
-  if(body.querySelector("#sub-tit > div.title_area > div > h3") != null) { // 게시판 여부
-    console.log("게시판 입니다."); 
-    
-    const notice = body.querySelectorAll("#upperArticleList > table > tbody > tr");
-    console.log(notice.length);    
+const iframe = document.getElementById('cafe_main');
 
-    for(let i = 0; i < notice.length; i++){
-      const row = notice[i];
-      console.log(row.querySelector("td.td_name > div > table > tbody > tr > td > a").textContent);            
+iframe.onload = function() {  
+  let body = iframeRef(iframe)
+  if(body.querySelector("#sub-tit > div.title_area > div > h3") != null) { // 게시판 여부    
+    const noticeList = body.querySelectorAll("#upperArticleList > table > tbody > tr");
+    
+    for(let i = 0; i < noticeList.length; i++){
+      const row = noticeList[i];
+      const nickname = row.querySelector("td.td_name > div > table > tbody > tr > td > a").textContent;
+   
+      switch(nickname) {
+        case "비밀의공구":
+          row.hidden = true;                      
+          break;
+      }
     }
     
   } else {
