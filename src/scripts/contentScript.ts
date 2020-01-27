fc90c080b226ff97218203cd31fc674d6a045d0a
cheongan <smartnation@naver.com>
2020-01-27 16:13:13 +0900

    feat: ⚡️ Add UserBlock in contentScript.ts

--- src/scripts/contentScript.ts
+++ src/scripts/contentScript.ts
@@ -4,14 +4,15 @@ const iframe = document.getElementById('cafe_main');
 
 iframe.onload = function() {  
   let body = iframeRef(iframe)
-
-  // browser.storage.local.get("kitten").then(gotKitten, onError);
-
-  if(body.querySelector("#sub-tit > div.title_area > div > h3") != null) { // 게시판 여부   
+  
+  const BoardTitle = body.querySelector("#sub-tit > div.title_area > div > h3") ;
+  if(BoardTitle != null) { // 게시판 여부                            
+    console.log(body.querySelector("#sub-tit > div.title_area > div > h3").textContent);
     
-    // 게시물 부분
+    let postList = body.querySelectorAll("#main-area > div:nth-child(7) > table > tbody > tr");
+    if(BoardTitle.textContent == "전체글보기") postList = body.querySelectorAll("#main-area > div:nth-child(6) > table > tbody > tr");
 
-    const postList = body.querySelectorAll("#main-area > div:nth-child(6) > table > tbody > tr");
+    
     for(let i = 0; i < postList.length; i++){
       const row = postList[i];
       const userElement = row.querySelector("td.td_name > div > table > tbody > tr > td > a");
@@ -20,13 +21,8 @@ iframe.onload = function() {
       userElement.addEventListener("click", () => {            
         const repeat = setInterval(() => {
           console.log("Repeat Ready");
-          if(body.querySelector("div.perid-layer > ul") != null) {            
-            clearInterval(repeat);
-            browser.storage.local.set({id}).then(() => {
-              console.log("OK");
-            }, (error) => {
-              console.log(error);
-            });
+          if(body.querySelector("div.perid-layer > ul") != null) {                        
+            clearInterval(repeat);            
             if(body.querySelector("li.blackUser") == null) {
               const blackElement = document.createElement('li')            
               blackElement.className = "blackUser";
@@ -37,7 +33,11 @@ iframe.onload = function() {
               console.log(listElement);          
             
               blackElement.addEventListener("click", () => {
-                blockUser(id);            
+                browser.storage.local.set({blockUsers: [id]}).then(() => {
+                  console.log(`Block ${id} OK`);
+                }, (error) => {
+                  console.log(error);
+                });
                 body.querySelector("div.perid-layer").style.display = "none";
               });
             }  
@@ -45,14 +45,19 @@ iframe.onload = function() {
         }, 100)
       });
 
-      // switch(nickname) {
-      //   case "하니":
-      //     row.hidden = true;                  
-      //     break;
-      // }
+      // 차단 부분
+      browser.storage.local.get("blockUsers").then((user) => {             
+        console.log(user);
+        
+        const users = user.blockUsers.filter(user => user == id);               
+        if (users.length > 0) {
+          row.hidden = true;
+          console.log(`${id} is Blocked!`);
+        }          
+      }, (err) => {
+        console.log(err);      
+      });
     }
-
-    // 게시물 부분 끝
     
     
   } else {
@@ -65,8 +70,3 @@ function iframeRef(frameRef) {
     ? frameRef.contentWindow.document
     : frameRef.contentDocument
 }
-
-function blockUser(id){
-  console.log(`User Blocked : ${id}`);  
-}
-
