3576797a4326eb855bde7e3d946281e67f144462
cheongan <smartnation@naver.com>
2020-01-27 13:32:02 +0900

    feat: ⚡️ Using storage in contentScript.ts

--- src/scripts/contentScript.ts
+++ src/scripts/contentScript.ts
@@ -1,9 +1,40 @@
+import browser from 'webextension-polyfill';
+
 const iframe = document.getElementById('cafe_main');
 
 iframe.onload = function() {  
   let body = iframeRef(iframe)
 
+  const value = "바보";
 
+  function setItem() {
+    console.log("OK");
+  }
+
+  function onError(error) {
+    console.log(error)
+  }
+
+  var monster = {
+    name: "Kraken",
+    tentacles: true,
+    eyeCount: 10
+  }
+  
+  var kitten = {
+    name: "Moggy",
+    tentacles: false,
+    eyeCount: 2
+  }
+
+
+  browser.storage.local.set({kitten, monster}).then(() => {
+
+  }, (error) => {
+    console.log(error);    
+  });
+
+  // browser.storage.local.get("kitten").then(gotKitten, onError);
 
   if(body.querySelector("#sub-tit > div.title_area > div > h3") != null) { // 게시판 여부   
     
@@ -12,25 +43,28 @@ iframe.onload = function() {
     const postList = body.querySelectorAll("#main-area > div:nth-child(6) > table > tbody > tr");
     for(let i = 0; i < postList.length; i++){
       const row = postList[i];
-      const nicknameElement = row.querySelector("td.td_name > div > table > tbody > tr > td > a");
-      const nickname = nicknameElement.textContent;
+      const userElement = row.querySelector("td.td_name > div > table > tbody > tr > td > a");
+      const id = userElement.getAttribute('onclick').split(",")[1].replace("\'", "");
 
-      nicknameElement.addEventListener("click", () => {                        
-        setTimeout(() => {
-          const blackElement = document.createElement('li')
-          blackElement.innerHTML = '<a href="#">사용자블락</a>';          
-          const listElement = body.querySelector("div.perid-layer > ul");                              
-          listElement.append(blackElement);
-          console.log(listElement);
-        }, 500);
+      console.log(id);
+      
+
+      // userElement.addEventListener("click", () => {                        
+      //   setTimeout(() => {
+      //     const blackElement = document.createElement('li')
+      //     blackElement.innerHTML = `<a href="#" onclick="blockUser(${nickname})">사용자블락</a>`;          
+      //     const listElement = body.querySelector("div.perid-layer > ul");                              
+      //     listElement.append(blackElement);
+      //     console.log(listElement);
+      //   }, 500);
           
-      });
+      // });
 
-      switch(nickname) {
-        case "하니":
-          row.hidden = true;                  
-          break;
-      }
+      // switch(nickname) {
+      //   case "하니":
+      //     row.hidden = true;                  
+      //     break;
+      // }
     }
 
     // 게시물 부분 끝
